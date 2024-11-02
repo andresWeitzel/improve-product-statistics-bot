d7bba54e0304d3c21e920bcd1e3e68c6fdef06c9
Andrés Weitzel <andres96energy@hotmail.com>
2024-11-02 05:26:04 -0300

    Update links

--- src/const/web.js
+++ src/const/web.js
@@ -1,4 +1,94 @@
 const urlsML = [
+  //======FACEBOOK=========
+  //TV lg
+  "https://www.facebook.com/marketplace/item/1621665825425193/",
+  //Teclado Teclado Noganet Nkb-78033 Multimedia Mini Usb Pc.
+  "https://www.facebook.com/marketplace/item/1609382393037932/",
+  //Vaso Chop De Cerveza Isenbeck
+  "https://www.facebook.com/marketplace/item/1065221695099345",
+  //Botines futbol 41.5
+  "https://www.facebook.com/marketplace/item/1206256507327685/",
+  //Switch Multiplicador De Puertos Hdmi 4k
+  "https://www.facebook.com/marketplace/item/1183912519357004/",
+  //Conversor Adaptador Convertidor Vga a HDMI con Audio
+  "https://www.facebook.com/marketplace/item/1584508075434854/",
+  //Microscopio Hokenn + kit
+  "https://www.facebook.com/marketplace/item/3000663360073580/",
+  //Libro Los secretos del ARTE CHINO
+  "https://www.facebook.com/marketplace/item/428519053589947/",
+  //Libro Vida, pensamiento y obra - Gauss
+  "https://www.facebook.com/marketplace/item/1043335753774609/",
+  //Libro Dickens - J.B Prietsley
+  "https://www.facebook.com/marketplace/item/27363243359933508/",
+  //Libro Crímenes para resolver
+  "https://www.facebook.com/marketplace/item/1626942424529610/",
+  //Libro El ingeniero de la luz y otros cuentos - Vallini
+  "https://www.facebook.com/marketplace/item/498694943038353/",
+  //Libro Recuerdos de otra vida - Mary Higgins Clark
+  "https://www.facebook.com/marketplace/item/1931826377262485/",
+  //Libro Las travesuras de naricita - Monteiro Lobato
+  "https://www.facebook.com/marketplace/item/2667008166840594/",
+  //Libro Conciencia - Osho
+  "https://www.facebook.com/marketplace/item/842344931212876/",
+  //Libro Ética para argentinos - Jaime Barylko
+  "https://www.facebook.com/marketplace/item/1660027931444783/",
+  //Libro Viaje al corazon del Mediterráneo - Ramon Villeró
+  "https://www.facebook.com/marketplace/item/1240115884080244/",
+  //Libro El Iniciado - Edith Canseco
+  "https://www.facebook.com/marketplace/item/1036456688229791/",
+  //Libro Ser humanos - Sergio Bregman
+  "https://www.facebook.com/marketplace/item/1045828050126529/",
+  //Libro Salvando al Soldado Ryan - Max Allan Collins
+  "https://www.facebook.com/marketplace/item/556582257034310/",
+  //Libro MICRO-ELECTRONICA - S. Gergely
+  "https://www.facebook.com/marketplace/item/989589302923428/",
+  //Libro El legado espiritual del templo shaolin - Andy Jammes
+  "https://www.facebook.com/marketplace/item/2919993271483328/",
+  //Libro Baloncesto - Dieter Niedich Esquemas y ejercicios tacticos
+  "https://www.facebook.com/marketplace/item/1073581677684245/",
+  //Libro shakespeare Comedias. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/551097280811344/",
+  //Libros de Artes Marciales. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/1231890337891152/",
+  //Libro Los principios y beneficios del cambio - Dr. Myles munroe
+  "https://www.facebook.com/marketplace/item/1583888609216169/",
+  //Libro El Universo Billiken. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/1440440199950783/",
+  //Libro Aprende A Programar Con Java. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/882844103760314/",
+  //Parlantes Braun Speaker Pc 180. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/848355540601865/",
+  //Antiguos Binoculares De Teatro 2.5 Green Japan. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/1551626932129023/",
+  //Colección Libros Agatha Christie. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/1691315851410333/",
+  //Gran Enciclopedia Del Saber National Geographic. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/3934018510200951/",
+  //Cuadernillos de Finales y Parciales Resueltos CBC. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/856785346443617/",
+  //Cuadernillos Biología CBC. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/1902122883631354/",
+  //Libro Romeo Y Julieta - William Shakespeare. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/1528848161399812/",
+  //Libro Gandhi - Louis Fischer (Original). LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/7812482412193589/",
+  //Antigua brujula militar Suiza, Plan LTD Neuchatel. I y II guerra mundial. (Original) LIQUIDACIÓN.
+  "https://www.facebook.com/marketplace/item/468152886119301/",
+  //Teclado Mecanico Gamer Hp Gaming. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/513397577904816/",
+  //Libro Criptografía Protección De Datos Y Aplicaciones ( Original Fisico ). LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/1202519147619127/",
+  //Libro Teoría De Control Para Informáticos - Fusario Crotti (Original-Físico). LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/538783181830410/",
+  //Raquetas de Tenis para entrenamiento. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/474743948597278/",
+  //Raqueta de Tenis Sufix Nano Energy. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/1423682634967378/",
+  //Raqueta de Tenis Prince Quantum powerline. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/510028034810255/",
+  //Oso gigante Phi Phi Toys
+  "https://www.facebook.com/marketplace/item/3724898594492637/",
+  //========MERCADOLIBRE================
   // Deportes
   "https://articulo.mercadolibre.com.ar/MLA-1446036033-raqueta-de-tenis-prince-quantum-powerline-liquidacion-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1446547373-raquetas-de-tenis-para-entrenamiento-liquidacion-_JM",
@@ -30,8 +120,8 @@ const urlsML = [
   "https://articulo.mercadolibre.com.ar/MLA-1910665652-coleccion-libros-agatha-christie-liquidacion-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1919187710-libros-de-artes-marciales-liquidacion-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1931256972-libro-shakespeare-comedias-liquidacion-_JM",
-  //Otros
-  "https://articulo.mercadolibre.com.ar/MLA-1450484881-oso-gigante-phi-phi-toys-_JM"
+  //peluches
+  "https://articulo.mercadolibre.com.ar/MLA-1450484881-oso-gigante-phi-phi-toys-_JM",
 ];
 
 const userAgents = [
