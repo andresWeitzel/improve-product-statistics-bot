32502891ce8b0e52b96383710cf1ea28dcf14823
Andrés Weitzel <andres96energy@hotmail.com>
2024-11-03 04:08:53 -0300

    Add prod-server

--- src/prod-server.js
+++ src/prod-server.js
@@ -7,13 +7,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+// Servir archivos estáticos desde la carpeta "public"
 app.use(express.static("public"));
 
 // Iniciar la función de visitas y pasar el objeto io
 incrementViewsML(io);
 
-// Iniciar el servidor
-const PORT=9008
+// Iniciar el servidor en el puerto especificado por Render
+const PORT = process.env.PORT || 9008; // 9008 es el puerto local por defecto
 server.listen(PORT, () => {
-  console.log("Servidor web en http://localhost:9008");
+  console.log(`Servidor web en http://localhost:${PORT}`);
 });
