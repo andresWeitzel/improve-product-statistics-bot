6f5e1e3256620052e4204887e9d0691895cfc877
Andrés Weitzel <andres96energy@hotmail.com>
2024-11-03 05:09:30 -0300

    Update puppeteer config

--- src/functions/prod/incrementViewsML.js
+++ src/functions/prod/incrementViewsML.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require("puppeteer-core");
 const { urlsML } = require("../../const/web");
 const {
   getNameFromUrlML,
@@ -11,8 +11,6 @@ let currentIndex = 0;
 let visitCounter = 0;
 
 async function incrementViewsML(io) {
-
-
   if (currentIndex >= urlsML.length) {
     currentIndex = 0;
     visitCounter++;
@@ -25,17 +23,22 @@ async function incrementViewsML(io) {
 
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: process.env.CHROME_BIN || undefined,
+    executablePath:
+      process.env.CHROME_BIN ||
+      "/opt/render/project/.cache/ms-playwright/chromium-*/chromium" ||
+      "/opt/render/project/src/node_modules/puppeteer-core/lib/cjs/puppeteer/node/BrowserLauncher" ||
+      "/usr/bin/chromium" ||
+      "/usr/bin/google-chrome",
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
       "--disable-dev-shm-usage",
       "--disable-accelerated-2d-canvas",
       "--disable-gpu",
-      "--window-size=1920,1080"
+      "--window-size=1920,1080",
     ],
   });
-  
+
   const page = await browser.newPage();
   await page.setUserAgent(getRandomUserAgent());
 
@@ -55,7 +58,9 @@ async function incrementViewsML(io) {
 
     await browser.close();
     logStatus(currentIndex + 1, "cerrada", nameProduct);
-    console.log("----------------------------------------------------------------");
+    console.log(
+      "----------------------------------------------------------------"
+    );
     currentIndex++;
     setTimeout(() => incrementViewsML(io), 2000); // Llamada recursiva con retardo
   }
