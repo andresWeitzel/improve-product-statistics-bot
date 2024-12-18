69ccbb37a36764d368384922217971f49928525a
Andrés Weitzel <andres96energy@hotmail.com>
2024-12-18 17:25:09 -0300

    update links

--- src/const/web.js
+++ src/const/web.js
@@ -1,19 +1,52 @@
 export const urlsML = [
+  //=======================
   //======FACEBOOK=========
-  //TV lg
-  "https://www.facebook.com/marketplace/item/1621665825425193/",
-  //Teclado Teclado Noganet Nkb-78033 Multimedia Mini Usb Pc.
-  "https://www.facebook.com/marketplace/item/1609382393037932/",
-  //Vaso Chop De Cerveza Isenbeck
-  "https://www.facebook.com/marketplace/item/1065221695099345",
+  //=======================
+  // ---- HOGAR -----
+  //Exahome Calefactor C1009. Liquidación
+  "https://www.facebook.com/marketplace/item/1278926506487660/",
+  // Biblioteca Estantería Mueble
+  "https://www.facebook.com/marketplace/item/1239493113974323/",
+    // ---- ROPA -----
+  //Pantalon De Entrenamiento Nike
+  "https://www.facebook.com/marketplace/item/596744276045862/",
+  //Pantalon Mma Reforzado Cruge
+  "https://www.facebook.com/marketplace/item/1667600670462978/",
+  // ---- DEPORTES -----
   //Botines futbol 41.5
   "https://www.facebook.com/marketplace/item/1206256507327685/",
+  // Botines de futbol 42
+  "https://www.facebook.com/marketplace/item/1266802967966158/",
+  //Raqueta de Tenis Prince Quantum powerline. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/510028034810255/",
+  //Multigimnasio Everlast. Liquidación
+  "https://www.facebook.com/marketplace/item/2465946693746733/",
+  // ---- PAINTBALL -----
+  //Chaleco Táctico De Paintball Grosstactical Con Ghillie.
+  "https://www.facebook.com/marketplace/item/564784833149028/",
+  //Máscara De Paintball Extreme Rage. Liquidación
+  "https://www.facebook.com/marketplace/item/1832940934199631/",
+  //Rodilleras Y Coderas Tácticas Paintball Airsoft Militar.
+  "https://www.facebook.com/marketplace/item/614897807667120/",
+  //Funda Casco Militar
+  "https://www.facebook.com/marketplace/item/1523389154982104/",
+  // ---- ANTIGUEDADES -----
+  //Vaso Chop De Cerveza Isenbeck
+  "https://www.facebook.com/marketplace/item/1065221695099345/",
+  //Cuadro De Lancha De Carrera Lucas Nicolini (60x80).
+  "https://www.facebook.com/marketplace/item/28800635122868768/",
+  // ---------- ELECTRONICA ----------
+  //TV lg
+  "https://www.facebook.com/marketplace/item/1621665825425193/",
+  //Teclado Noganet Mini Usb Pc.
+  "https://www.facebook.com/marketplace/item/1609382393037932/",
   //Switch Multiplicador De Puertos Hdmi 4k
   "https://www.facebook.com/marketplace/item/1183912519357004/",
   //Conversor Adaptador Convertidor Vga a HDMI con Audio
   "https://www.facebook.com/marketplace/item/1584508075434854/",
-  //Microscopio Hokenn + kit
-  "https://www.facebook.com/marketplace/item/3000663360073580/",
+  //Parlantes Braun Speaker Pc 180. LIQUIDACIÓN
+  "https://www.facebook.com/marketplace/item/848355540601865/",
+  // ------------- LIBROS -------------------------
   //Libro Los secretos del ARTE CHINO
   "https://www.facebook.com/marketplace/item/428519053589947/",
   //Libro Vida, pensamiento y obra - Gauss
@@ -56,10 +89,6 @@ export const urlsML = [
   "https://www.facebook.com/marketplace/item/1440440199950783/",
   //Libro Aprende A Programar Con Java. LIQUIDACIÓN
   "https://www.facebook.com/marketplace/item/882844103760314/",
-  //Parlantes Braun Speaker Pc 180. LIQUIDACIÓN
-  "https://www.facebook.com/marketplace/item/848355540601865/",
-  //Antiguos Binoculares De Teatro 2.5 Green Japan. LIQUIDACIÓN
-  "https://www.facebook.com/marketplace/item/1551626932129023/",
   //Colección Libros Agatha Christie. LIQUIDACIÓN
   "https://www.facebook.com/marketplace/item/1691315851410333/",
   //Gran Enciclopedia Del Saber National Geographic. LIQUIDACIÓN
@@ -72,49 +101,67 @@ export const urlsML = [
   "https://www.facebook.com/marketplace/item/1528848161399812/",
   //Libro Gandhi - Louis Fischer (Original). LIQUIDACIÓN
   "https://www.facebook.com/marketplace/item/7812482412193589/",
-  //Antigua brujula militar Suiza, Plan LTD Neuchatel. I y II guerra mundial. (Original) LIQUIDACIÓN.
-  "https://www.facebook.com/marketplace/item/468152886119301/",
-  //Teclado Mecanico Gamer Hp Gaming. LIQUIDACIÓN
-  "https://www.facebook.com/marketplace/item/513397577904816/",
   //Libro Criptografía Protección De Datos Y Aplicaciones ( Original Fisico ). LIQUIDACIÓN
   "https://www.facebook.com/marketplace/item/1202519147619127/",
-  //Libro Teoría De Control Para Informáticos - Fusario Crotti (Original-Físico). LIQUIDACIÓN
-  "https://www.facebook.com/marketplace/item/538783181830410/",
-  //Raqueta de Tenis Prince Quantum powerline. LIQUIDACIÓN
-  "https://www.facebook.com/marketplace/item/510028034810255/",
+  // ------------- OTROS -------------------------------
+  //Microscopio Hokenn + kit
+  "https://www.facebook.com/marketplace/item/3000663360073580/",
   //Oso gigante Phi Phi Toys
   "https://www.facebook.com/marketplace/item/3724898594492637/",
+  "https://www.facebook.com/marketplace/item/1342552800443424/",
+
+  //====================================
   //========MERCADOLIBRE================
+  //====================================
+  //Hogar
+  "https://articulo.mercadolibre.com.ar/MLA-1969047380-exahome-calefactor-c1009-liquidacion-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1464603961-biblioteca-estanteria-mueble-de-madera-grande-liquidacion-_JM",
   // Deportes
   "https://articulo.mercadolibre.com.ar/MLA-1446036033-raqueta-de-tenis-prince-quantum-powerline-liquidacion-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1931372874-botines-de-futbol-adidas-nro-415-liquidacion-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1960770844-botines-de-futbol-adidas-nro-42-liquidacion-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1974858594-pistola-co2-crosman-c21-lubric-balines-garrafa-otros-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1464709185-pistola-aire-comprimido-sp2-45mm-leer-detalle-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1974079398-multigimnasio-everlast-liquidacion-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1448937301-mira-telescopica-cannon-deluxe-usa-4x20-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1910681252-mira-optica-antigua-arce-liquidacion-_JM",
+  //Ropa
+  "https://articulo.mercadolibre.com.ar/MLA-1467929807-pantalon-de-entrenamiento-nike-original-liquidacion-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1467905421-pantalon-mma-reforzado-cruge-liquidacion-_JM",
+  //Paintball
+  "https://articulo.mercadolibre.com.ar/MLA-1464489677-mascara-de-paintball-extreme-rage-liquidacion-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1464771653-chaleco-tactico-de-paintball-grosstactical-con-ghillie-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1974852660-rodilleras-y-coderas-tacticas-paintball-airsoft-militar-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1975438202-pods-paintball-empire-gen-global-otros-x6-liquidacion-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1975569938-chaleco-tactico-de-paintball-liquidacion-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1973680454-funda-casco-militar-reversible-con-ghillie-liquidacion-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1976413104-marcadora-de-paintball-tippmann-a-5-stealth-customizada-_JM",
+  // Ropa
+  "https://articulo.mercadolibre.com.ar/MLA-1464985993-correaje-militar-de-ejercito-liquidacion-_JM",
   // Antiguedades-coleccion
-  "https://articulo.mercadolibre.com.ar/MLA-1446952039-antigua-brujula-militar-suiza-plan-ltd-neuchatel-_JM",
-  "https://articulo.mercadolibre.com.ar/MLA-1899107620-antiguos-binoculares-de-teatro-25-green-japan-liquidacion-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1456215693-vaso-chop-de-cerveza-isenbeck-importado-de-coleccion-1l-_JM",
-  // Teclados
+  "https://articulo.mercadolibre.com.ar/MLA-1969047150-cuadro-de-lancha-de-carrera-lucas-nicolini-60x80-_JM",
+  // Electronica
   "https://articulo.mercadolibre.com.ar/MLA-1939367090-teclado-noganet-nkb-78033-multimedia-mini-usb-pc-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1457973059-teclado-noganet-nkb-78033-multimedia-mini-usb-pc-color-del-teclado-negro-idioma-espanol-latinoamerica-_JM",
-  "https://articulo.mercadolibre.com.ar/MLA-1909456266-teclado-mecanico-gamer-hp-gaming-liquidacion-_JM",
-  // PC
   "https://articulo.mercadolibre.com.ar/MLA-1456258309-televisor-lg-liquidacion-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1929869212-conversor-adaptador-convertidor-vga-a-hdmi-con-audio-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1454365963-switch-multiplicador-de-puertos-hdmi-4k-de-3-puertos-en-1-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1899211176-parlante-braun-speaker-pc-180-liquidacion-_JM",
-  // Miras
-  "https://articulo.mercadolibre.com.ar/MLA-1448937301-mira-telescopica-cannon-deluxe-usa-4x20-_JM",
-  "https://articulo.mercadolibre.com.ar/MLA-1910681252-mira-optica-antigua-arce-liquidacion-_JM",
-  // Libros
+  // // Libros
   "https://articulo.mercadolibre.com.ar/MLA-1446900191-libro-criptografia-proteccion-de-datos-y-aplicaciones-_JM",
-  "https://articulo.mercadolibre.com.ar/MLA-1898204944-libro-teoria-de-control-para-informaticos-fusario-alfaomega-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1910615436-libro-los-principios-y-beneficios-del-cambio-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1448849697-libro-aprende-a-programar-con-java-liquidacion-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1449990329-gran-enciclopedia-del-saber-national-geographic-liquidacion-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1910665652-coleccion-libros-agatha-christie-liquidacion-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1919187710-libros-de-artes-marciales-liquidacion-_JM",
   "https://articulo.mercadolibre.com.ar/MLA-1931256972-libro-shakespeare-comedias-liquidacion-_JM",
-  //peluches
+  "https://articulo.mercadolibre.com.ar/MLA-1464652905-libro-los-secretos-del-arte-chino-formato-fisico-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1464560549-libro-vida-pensamiento-y-obra-gauss-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1464640421-libro-dickens-jb-prietsley-_JM",
+  // Otros
   "https://articulo.mercadolibre.com.ar/MLA-1450484881-oso-gigante-phi-phi-toys-_JM",
+  "https://articulo.mercadolibre.com.ar/MLA-1465476865-peluche-oso-phi-phi-toys-liquidacion-_JM"
 ];
 
 export const userAgents = [
